f6e807a846278097e14c67b94d8f4b1094f50f69
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-08-12 14:33:08 +0700

    [Add] global validation pipe, class-tranformer

--- src/components/cars/dto/inputs/new-car.input.ts
+++ src/components/cars/dto/inputs/new-car.input.ts
@@ -1,10 +1,14 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsNotEmpty, Max, Min } from 'class-validator';
 
 @InputType()
 export class NewCarInput {
   @Field()
+  @IsNotEmpty()
   name: string;
   @Field((type) => Int)
+  @Min(1000)
+  @Max(30000)
   price: number;
   @Field({ nullable: true })
   gas: string;
