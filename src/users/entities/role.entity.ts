2d49b51182aab0f9f1af965828b23e9ff89f4fa3
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-08-17 17:38:59 +0700

    [Update] mutation can create user now

--- src/users/entities/role.entity.ts
+++ src/users/entities/role.entity.ts
@@ -1,12 +1,17 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { User } from './users.entity';
 
 @Entity({ name: 'role' })
+@ObjectType()
 export class Role {
   @PrimaryGeneratedColumn('increment')
+  @Field((type) => Int)
   roleId: number;
   @Column()
+  @Field()
   roleName: string;
   @OneToMany((type) => User, (user) => user.userId)
+  @Field((type) => [User])
   users: User[];
 }
