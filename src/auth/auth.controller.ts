d29f002511e0098b80546df4548450d812771a9f
Prakit Wongnuntananon <boatnakrub_@hotmail.com>
2021-08-17 22:35:47 +0700

    [Update] after login success client will have auth token cookie

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
+import { User } from 'src/users/entities/users.entity';
+import { AuthService } from './auth.service';
+
+type token = {
+  access_token: string;
+  refresh_token: string;
+};
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  public login(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ): string {
+    const access_token = this.authService.generateToken(req.user as User);
+    const tokens = {
+      access_token,
+      refresh_token: '',
+    };
+    res.cookie('auth-token', tokens);
+    return 'login success';
+  }
+}
